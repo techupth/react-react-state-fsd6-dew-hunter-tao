a72dafc5a9db702c27c972faa38c58be18afae28
Surakit <surakitkunkayun@gmail.com>
2024-06-05 11:15:39 +0700

    feat: finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+const [greetingMessage,setGreetingMessage] = useState("Greeting Message");
+const switchToThai = () => {
+  setGreetingMessage("สวัสดี!");
+};
+const switchToEng = () => {
+  setGreetingMessage("Hi!");
+};
+const switchToCh = () => {
+  setGreetingMessage("你好!");
+};
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick ={switchToThai}>สวัสดี!</button>
+        <button onClick ={switchToEng}>Hi!</button>
+        <button onClick ={switchToCh}>你好!</button>
       </div>
     </div>
   );
